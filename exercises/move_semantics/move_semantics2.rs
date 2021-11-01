4328a539c10dcdcb38d1cdce31cd9dbd6c3953e6
Gleichlucas <lucas.kunz94@gmail.com>
2021-11-01 19:17:45 +0100

    move_semantics

--- exercises/move_semantics/move_semantics2.rs
+++ exercises/move_semantics/move_semantics2.rs
@@ -5,24 +5,22 @@
 // I AM NOT DONE
 
 fn main() {
-    let vec0 = Vec::new();
+    let mut vec0 = Vec::new();
 
-    let mut vec1 = fill_vec(vec0);
-
+    vec0.push(22);
+    vec0.push(44);
+    vec0.push(66);
+    let mut vec1 = fill_vec(&vec0);
+ 
     // Do not change the following line!
     println!("{} has length {} content `{:?}`", "vec0", vec0.len(), vec0);
-
-    vec1.push(88);
+ 
+    vec1.push(88); 
 
     println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
 }
 
-fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
-    let mut vec = vec;
-
-    vec.push(22);
-    vec.push(44);
-    vec.push(66);
-
+fn fill_vec(vec_ref: &Vec<i32>) -> Vec<i32> {
+    let mut vec = vec_ref.clone();
     vec
 }
